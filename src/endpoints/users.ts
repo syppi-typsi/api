c41d39953abe5855784c26c57bee04b5e96136a1
Oliver Jõgar <me@treier.xyz>
2024-04-22 22:04:52 +0300

    Very basic query
    See #3

--- src/endpoints/users.ts
+++ src/endpoints/users.ts
@@ -1,7 +1,13 @@
 import { Hono } from "hono";
+import { query } from "../db";
 
 const app = new Hono();
 
 app.get("/", (c) => c.json("This is /users"));
+app.get("/:id", async (c) => {
+	const id = c.req.param("id");
+	const res = await query("SELECT * FROM users WHERE id = $1", [id]);
+	return c.json(res);
+});
 
 export default app;
