0c12590970d5d8a3bf55a62a8e363cdf68784257
Oliver Jõgar <me@treier.xyz>
2024-05-05 19:27:01 +0300

    Correct and format tables for merge

--- src/db/tables.ts
+++ src/db/tables.ts
@@ -1,55 +1,47 @@
-import { query} from "./index";
+import { query } from "./index";
 
+export const execute = async () => {
+	try {
+		await query(usersTable, []);
+		console.log('If does not exist, table "users" is created');
+	} catch (error) {
+		console.error(error);
+	}
 
-export const execute = async() => {
-    try {
-        const res = await query(usersTable, []);
-        console.log('If does not exists, table "users" is created');
-    } catch (error) {
-        console.error(error);
-    }
+	try {
+		await query(placesTable, []);
+		console.log('If does not exist, table "places" is created');
+	} catch (error) {
+		console.error(error);
+	}
 
-    try {
-        const res = await query(placesTable, []);
-        console.log('If does not exists, table "places" is created');
-    } catch (error) {
-        console.error(error);
-    }
+	try {
+		await query(categoryTable, []);
+		console.log('If does not exist, table "category" is created');
+	} catch (error) {
+		console.error(error);
+	}
 
-    try {
-        const res = await query(categoryTable, []);
-        console.log('If does not exists, table "category" is created');
-    } catch (error) {
-        console.error(error);
-    }
+	try {
+		await query(drinkTable, []);
+		console.log('If does not exist, table "drink" is created');
+	} catch (error) {
+		console.error(error);
+	}
 
-    try {
-        const res = await query(typeEnum, []);
-        console.log('If does not exists, enum "type" is created');
-    } catch (error) {
-        console.error(error);
-    }
+	try {
+		await query(ratingsTable, []);
+		console.log('If does not exist, table "ratings" is created');
+	} catch (error) {
+		console.error(error);
+	}
 
-    try {
-        const res = await query(drinkTable, []);
-        console.log('If does not exists, table "drink" is created');
-    } catch (error) {
-        console.error(error);
-    }
-
-    try {
-        const res = await query(ratingsTable, []);
-        console.log('If does not exists, table "ratings" is created');
-    } catch (error) {
-        console.error(error);
-    }
-
-    try {
-        const res = await query(alterTables, []);
-        console.log('If not already, tables are altered');
-    } catch (error) {
-        console.error(error);
-    }
+	try {
+		await query(alterTables, []);
+		console.log("If not already, tables are altered");
+	} catch (error) {
+		console.error(error);
+	}
 };
 
 const usersTable = `CREATE TABLE IF NOT EXISTS "users" (
@@ -76,13 +68,6 @@ const categoryTable = `CREATE TABLE IF NOT EXISTS "category" (
     PRIMARY KEY("id")
     );`;
 
-const typeEnum = `
-    DO $$ BEGIN
-        CREATE TYPE "volumes_t" AS ENUM ();
-    EXCEPTION
-        WHEN duplicate_object THEN null;
-    END $$;`;
-
 const drinkTable = `CREATE TABLE IF NOT EXISTS "drink" (
     "id" serial NOT NULL,
     "name" varchar(255) NOT NULL,
@@ -91,8 +76,8 @@ const drinkTable = `CREATE TABLE IF NOT EXISTS "drink" (
     "description" text,
     "product_image" varchar(255),
     "category" int NOT NULL,
-    "added_on" TIMESTAMPTZ NOT NULL,
-    "volumes" volumes_t[],
+    "added_on" TIMESTAMPTZ NOT NULL DEFAULT now(),
+    "volumes" integer[],
     "abv" float,
     "places" int,
     "nutritional_value" json,
@@ -115,7 +100,7 @@ const alterTables = `
         FOREIGN KEY("places") REFERENCES "places"("id")
         ON UPDATE CASCADE ON DELETE CASCADE;
     EXCEPTION
-        WHEN duplicate_table THEN  -- postgres raises duplicate_table at surprising times. Ex.: for UNIQUE constraints.
+        WHEN duplicate_table THEN
         WHEN duplicate_object THEN
         RAISE NOTICE 'Table constraint drink_place already exists';
     END;
@@ -126,7 +111,7 @@ const alterTables = `
         FOREIGN KEY("category") REFERENCES "category"("id")
         ON UPDATE CASCADE ON DELETE CASCADE;
     EXCEPTION
-        WHEN duplicate_table THEN  -- postgres raises duplicate_table at surprising times. Ex.: for UNIQUE constraints.
+        WHEN duplicate_table THEN
         WHEN duplicate_object THEN
         RAISE NOTICE 'Table constraint drink_category already exists';
     END;
@@ -137,7 +122,7 @@ const alterTables = `
         FOREIGN KEY("parent") REFERENCES "category"("id")
         ON UPDATE CASCADE ON DELETE CASCADE;
     EXCEPTION
-        WHEN duplicate_table THEN  -- postgres raises duplicate_table at surprising times. Ex.: for UNIQUE constraints.
+        WHEN duplicate_table THEN
         WHEN duplicate_object THEN
         RAISE NOTICE 'Table constraint category_parent already exists';
     END;
@@ -148,7 +133,7 @@ const alterTables = `
         FOREIGN KEY("drink") REFERENCES "drink"("id")
         ON UPDATE NO ACTION ON DELETE NO ACTION;
     EXCEPTION
-        WHEN duplicate_table THEN  -- postgres raises duplicate_table at surprising times. Ex.: for UNIQUE constraints.
+        WHEN duplicate_table THEN
         WHEN duplicate_object THEN
         RAISE NOTICE 'Table constraint rating_drink already exists';
     END;
@@ -159,10 +144,9 @@ const alterTables = `
         FOREIGN KEY("user") REFERENCES "users"("id")
         ON UPDATE NO ACTION ON DELETE NO ACTION;
     EXCEPTION
-        WHEN duplicate_table THEN  -- postgres raises duplicate_table at surprising times. Ex.: for UNIQUE constraints.
+        WHEN duplicate_table THEN
         WHEN duplicate_object THEN
         RAISE NOTICE 'Table constraint rating_user already exists';
     END;
 
     END $$;`;
-
