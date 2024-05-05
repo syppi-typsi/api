c3c17b7b79a8f053bdc80ce34b65153fe947a69e
MatiasJurgenson <jurgenson.matias@gmail.com>
2024-05-05 13:16:18 +0300

    fix sql error messages

--- src/db/tables.ts
+++ src/db/tables.ts
@@ -76,7 +76,12 @@ const categoryTable = `CREATE TABLE IF NOT EXISTS "category" (
     PRIMARY KEY("id")
     );`;
 
-const typeEnum = `CREATE TYPE "volumes_t" AS ENUM ();`;
+const typeEnum = `
+    DO $$ BEGIN
+        CREATE TYPE "volumes_t" AS ENUM ();
+    EXCEPTION
+        WHEN duplicate_object THEN null;
+    END $$;`;
 
 const drinkTable = `CREATE TABLE IF NOT EXISTS "drink" (
     "id" serial NOT NULL,
@@ -100,28 +105,64 @@ const ratingsTable = `CREATE TABLE IF NOT EXISTS "ratings" (
     "rating" int
     );`;
 
-const alterTables = `ALTER TABLE "drink" 
-    ADD CONSTRAINT "drink_place"
-    FOREIGN KEY("places") REFERENCES "places"("id")
-    ON UPDATE CASCADE ON DELETE CASCADE;
+const alterTables = `
+    DO $$
+    BEGIN
 
-    ALTER TABLE "drink"
-    ADD CONSTRAINT "drink_category"
-    FOREIGN KEY("category") REFERENCES "category"("id")
-    ON UPDATE CASCADE ON DELETE CASCADE;
+    BEGIN
+        ALTER TABLE "drink" 
+        ADD CONSTRAINT "drink_place"
+        FOREIGN KEY("places") REFERENCES "places"("id")
+        ON UPDATE CASCADE ON DELETE CASCADE;
+    EXCEPTION
+        WHEN duplicate_table THEN  -- postgres raises duplicate_table at surprising times. Ex.: for UNIQUE constraints.
+        WHEN duplicate_object THEN
+        RAISE NOTICE 'Table constraint drink_place already exists';
+    END;
 
-    ALTER TABLE "category"
-    ADD CONSTRAINT "category_parent"
-    FOREIGN KEY("parent") REFERENCES "category"("id")
-    ON UPDATE CASCADE ON DELETE CASCADE;
+    BEGIN
+        ALTER TABLE "drink"
+        ADD CONSTRAINT "drink_category"
+        FOREIGN KEY("category") REFERENCES "category"("id")
+        ON UPDATE CASCADE ON DELETE CASCADE;
+    EXCEPTION
+        WHEN duplicate_table THEN  -- postgres raises duplicate_table at surprising times. Ex.: for UNIQUE constraints.
+        WHEN duplicate_object THEN
+        RAISE NOTICE 'Table constraint drink_category already exists';
+    END;
 
-    ALTER TABLE "ratings"
-    ADD CONSTRAINT "rating_drink"
-    FOREIGN KEY("drink") REFERENCES "drink"("id")
-    ON UPDATE NO ACTION ON DELETE NO ACTION;
+    BEGIN
+        ALTER TABLE "category"
+        ADD CONSTRAINT "category_parent"
+        FOREIGN KEY("parent") REFERENCES "category"("id")
+        ON UPDATE CASCADE ON DELETE CASCADE;
+    EXCEPTION
+        WHEN duplicate_table THEN  -- postgres raises duplicate_table at surprising times. Ex.: for UNIQUE constraints.
+        WHEN duplicate_object THEN
+        RAISE NOTICE 'Table constraint category_parent already exists';
+    END;
 
-    ALTER TABLE "ratings"
-    ADD CONSTRAINT "rating_user"
-    FOREIGN KEY("user") REFERENCES "users"("id")
-    ON UPDATE NO ACTION ON DELETE NO ACTION;`;
+    BEGIN
+        ALTER TABLE "ratings"
+        ADD CONSTRAINT "rating_drink"
+        FOREIGN KEY("drink") REFERENCES "drink"("id")
+        ON UPDATE NO ACTION ON DELETE NO ACTION;
+    EXCEPTION
+        WHEN duplicate_table THEN  -- postgres raises duplicate_table at surprising times. Ex.: for UNIQUE constraints.
+        WHEN duplicate_object THEN
+        RAISE NOTICE 'Table constraint rating_drink already exists';
+    END;
+
+    BEGIN
+        ALTER TABLE "ratings"
+        ADD CONSTRAINT "rating_user"
+        FOREIGN KEY("user") REFERENCES "users"("id")
+        ON UPDATE NO ACTION ON DELETE NO ACTION;
+    EXCEPTION
+        WHEN duplicate_table THEN  -- postgres raises duplicate_table at surprising times. Ex.: for UNIQUE constraints.
+        WHEN duplicate_object THEN
+        RAISE NOTICE 'Table constraint rating_user already exists';
+    END;
+
+    END $$;`;
 
